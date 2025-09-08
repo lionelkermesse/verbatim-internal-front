070c8f3d0e4b109947b601a17380d99ca699446f
Lionel KEMINSE <lionelkermesse@gmail.com>
2025-09-08 08:35:35 +0200

    initializing of project with core components

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "none",
+  "printWidth": 150,
+  "tabWidth": 2,
+  "arrowParens": "avoid",
+  "bracketSpacing": true
+}
